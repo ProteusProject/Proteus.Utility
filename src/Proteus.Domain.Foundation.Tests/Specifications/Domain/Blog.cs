e2cf792bcfa72be0d3d51baf9e3b1b4b4e55512d
Steve Bohlen <sbohlen@gmail.com>
2010-11-29 03:20:08 +0000

    added copyright notices to Specification-related source files
    git-svn-id: http://proteusproject.googlecode.com/svn/trunk@25 c5564b36-f9fc-11dd-99c0-75d572ba1ddd

--- src/Proteus.Domain.Foundation.Tests/Specifications/Domain/Blog.cs
+++ src/Proteus.Domain.Foundation.Tests/Specifications/Domain/Blog.cs
@@ -1,3 +1,29 @@
+/*
+ *
+ * Proteus
+ * Copyright (C) 2010
+ * Stephen A. Bohlen
+ * http://www.unhandled-exceptions.com
+ *
+ * Portions Copyright (C) 2008
+ * Steve Burman
+ * Linq.Specifications http://code.google.com/p/linq-specifications/
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+
+
 using Proteus.Domain.Foundation.Tests.Specifications.Domain.Base;
 
 namespace Proteus.Domain.Foundation.Tests.Specifications.Domain
