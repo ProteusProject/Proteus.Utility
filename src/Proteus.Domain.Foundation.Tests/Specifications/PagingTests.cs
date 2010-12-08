8aedf453c3b30b93e1dc567b8946d33c44db088f
Steve Bohlen <sbohlen@gmail.com>
2010-12-08 01:18:29 +0000

    Introduced initial paging tests for existing behavior
    git-svn-id: http://proteusproject.googlecode.com/svn/trunk@31 c5564b36-f9fc-11dd-99c0-75d572ba1ddd

---
+++ src/Proteus.Domain.Foundation.Tests/Specifications/PagingTests.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Proteus.Domain.Foundation.Specifications;
+using System.Linq.Expressions;
+
+
+namespace Proteus.Domain.Foundation.Tests.Specifications
+{
+    [TestFixture]
+    public class PagingTests
+    {
+        private IList<int> _numbers;
+
+        [TestFixtureSetUp]
+        public void _TestTestFixtureSetUp()
+        {
+            _numbers = new List<int>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                _numbers.Add(i);
+            }
+
+        }
+
+        [Test]
+        public void EvenNumbersSpecification_Returns_Only_Even_Members()
+        {
+            var spec = new EvenNumbersSpecification();
+
+            var results = spec.SatisfyingElementsFrom(_numbers.AsQueryable());
+
+            foreach (var item in results)
+            {
+                Assert.That(item % 2 == 0);
+            }
+        }
+
+        [Test]
+        public void Can_Page_Results()
+        {
+            var spec = new EvenNumbersSpecification();
+            var results = spec.SatisfyingElementsFrom(_numbers.AsQueryable());
+
+            Assert.That(results.Count(), Is.EqualTo(10));
+        }
+
+    }
+
+    public class EvenNumbersSpecification : Specification<int>
+    {
+
+        public override Expression<Func<int, bool>> Predicate
+        {
+            get { return t => t % 2 == 0; }
+        }
+
+        public override int? NumberOfResults
+        {
+            get
+            {
+                return 10;
+            }
+        }
+    }
+
+}
