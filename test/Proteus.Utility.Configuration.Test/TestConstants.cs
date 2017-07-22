cf3c92a1620d491f7230506042778bb0622d4e12
Steve Bohlen <sbohlen@gmail.com>
2017-07-22 06:38:18 -0400

    refactoring

---
+++ test/Proteus.Utility.Configuration.Test/TestConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace Proteus.Utility.Configuration.Test.ConnectionStringsTests
+{
+    public static class TestConstants
+    {
+        public const string ConnectionStringTestKey = "theTestKey";
+        public const string ConnectionStringFromAppConfig = "the-connection-string-from-app.config-file";
+        public const string ProviderNameFromAppConfig = "the-provider-name-from-app.config-file";
+
+        public const string AppSettingsValueFromAppEnvironmentVariable = "value-read-from-environment";
+        public const string AppSettingTestKey = "theTestKey";
+        public const string AppSettingsValueFromAppConfigFile = "value-read-from-app.config-file";
+    }
+}
