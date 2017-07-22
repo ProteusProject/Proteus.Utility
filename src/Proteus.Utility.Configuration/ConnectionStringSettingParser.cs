c86725c905fb5ed808d16c6f98e413919aa713a4
Steve Bohlen <sbohlen@gmail.com>
2017-07-22 09:57:12 -0400

    introducing skeleton of ConnectionStringSettingParser and corresponding failing tests

---
+++ src/Proteus.Utility.Configuration/ConnectionStringSettingParser.cs
@@ -0,0 +1,31 @@
+﻿using System.Configuration;
+
+namespace Proteus.Utility.Configuration
+{
+    public static class ConnectionStringSettingParser
+    {
+        public static ConnectionStringSettings Parse(string connectionSettingString)
+        {
+            return new ConnectionStringSettings(
+                ParseNameFrom(connectionSettingString),
+                ParseConnectionStringFrom(connectionSettingString),
+                ParseProviderNameFrom(connectionSettingString)
+                );
+        }
+
+        private static string ParseProviderNameFrom(string connectionSettingString)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static string ParseConnectionStringFrom(string connectionSettingString)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static string ParseNameFrom(string connectionSettingString)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ test/Proteus.Utility.Configuration.Test/ConnectionStringsTests/WhenParsingCompleteConnectionStringValue.cs
@@ -0,0 +1,36 @@
+﻿using System.Configuration;
+using NUnit.Framework;
+
+namespace Proteus.Utility.Configuration.Test.ConnectionStringsTests
+{
+    [TestFixture]
+    public class WhenParsingCompleteConnectionStringValue
+    {
+        private ConnectionStringSettings _connectionStringSetting;
+
+        [SetUp]
+        public void TestSetUp()
+        {
+            _connectionStringSetting = ConnectionStringSettingParser.Parse(TestConstants.ConnectionStringValueOfEnvironmentVariable);
+        }
+
+
+        [Test]
+        public void CanParseConnectionName()
+        {
+            Assert.That(_connectionStringSetting.Name, Is.EqualTo(TestConstants.ConnectionStringTestKey));
+        }
+
+        [Test]
+        public void CanParseConnectionString()
+        {
+            Assert.That(_connectionStringSetting.ConnectionString, Is.EqualTo(TestConstants.ConnectionStringValueFromEnvironmentVariable));
+        }
+
+        [Test]
+        public void CanParseProviderName()
+        {
+            Assert.That(_connectionStringSetting.Name, Is.EqualTo(TestConstants.ConnectionStringProviderNameValueFromEnvironmentVariable));
+        }
+    }
+}
